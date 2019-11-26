39cc3b50d8c729df168ebfd2061cfc3798947c24
Oleksandr Lebedko <Oleksandr_Lebedko@epam.com>
2019-11-26 15:12:51 +0200

    Add auto modification for unix like line breakers

--- ux-start-up.sh
+++ ux-start-up.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sed -i 's/\r//' init-db.sh
+
 docker build --tag cra2-psql .
 
 docker run --name cra2-psql-container -p 5432:5432 -v postgres-data:/var/lib/postgresql/data cra2-psql
--- win-start-up.bat
+++ win-start-up.bat
@@ -1,3 +1,5 @@
+sed -i 's/\r//' init-db.sh
+
 docker build --tag cra2-psql .
 
-docker run --name cra2-psql-container -p 5432:5432 -v postgres-data:/var/lib/postgresql/data cra2-psql
+docker run --name cra2-psql-container -p 5432:5432 -v postgres-data:/var/lib/postgresql/data cra2-psql 
